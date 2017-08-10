a7e70bff2db5ac21fb316542a57d9c371d43f914
Nathan Andrew Miller <nathanm@lanl.gov>
2017-08-10 15:48:06 -0600

    Initial commit

---
+++ src/cpp/tensor.h
@@ -0,0 +1,23 @@
+/*=======================================================
+  |                                                     |
+  |                     tensor.h                        |
+  |                                                     |
+  -------------------------------------------------------
+  | The header file for a class definition that allows  |
+  | n-dimensional tensors. The data for the tensor is   |
+  | stored in a 2 dimensional array and there is logic  |
+  | which allows referencing to the correct indices to  |
+  | occur.                                              |
+  =======================================================
+  | Dependencies:                                       |
+  | Eigen: An implementation of various matrix          |
+  |        commands. The implementation of the data     |
+  |        matrix uses such a matrix.                   |
+  =======================================================*/
+  
+namespace tensor
+{
+    class Tensor{
+        /*The base class for the tensor implementation*/
+    }
+}
