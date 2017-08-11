cabbf425589a86f36425de7193c194254115274b
Nathan Andrew Miller <nathanm@lanl.gov>
2017-08-10 22:16:57 -0600

    Initial commit

---
+++ src/cpp/compile_tests.py
@@ -0,0 +1,20 @@
+###========================================
+#|                                      |
+#|         compile_tests.py             |
+#|                                      |
+#========================================
+#| Compiles the tests of the individual |
+#| functions which can be tested        |
+#| independently of the overall code.   |
+#|                                      |
+#| These tests should output a LaTeX    |
+#| file "results.tex" which contains    |
+#| function/method name & True/False    |
+#| where True indicates that the method |
+#| or function passed the test and      |
+#| False indicates the converse.        |
+#========================================"""
+
+import os
+import sys
+
