abb342fb1bdaf58c6ac5c736a63be081010806da
Nathan Andrew Miller <nathanm@lanl.gov>
2017-08-10 21:56:09 -0600

    Initial commit

---
+++ src/cpp/tests/tensor/test_tensor.cpp
@@ -0,0 +1,42 @@
+/*!=======================================================
+  |                                                     |
+  |                 test_tensor.cpp                     |
+  |                                                     |
+  -------------------------------------------------------
+  | The unit test file for tensor.h/cpp.                |
+  | This file tests the classes and functions defined   |
+  | in tensor.h/cpp.                                    |
+  |                                                     |
+  | Generated files:                                    |
+  |    Results.tex:  A LaTeX file which contains the    |
+  |                  results as they will be included   |
+  |                  in the generated report.           |
+  =======================================================
+  | Dependencies:                                       |
+  | Eigen: An implementation of various matrix          |
+  |        commands. The implementation of the data     |
+  |        matrix uses such a matrix.                   |
+  =======================================================*/
+  
+#include <functional>
+#include <iostream>
+#include <fstream>
+#include <numeric>
+#include <vector>
+#include <cstdarg>
+#include <Eigen/Dense>
+#include <tensor.h>
+
+int main(){
+    /*!==========================
+    |         main            |
+    ===========================
+    
+    The main loop which runs the tests defined in the 
+    accompanying functions. Each function should output
+    the function name followed by & followed by True or 
+    False if the test passes or fails respectively.*/
+    
+    std::cout << "Compiled successfully";
+}
+
