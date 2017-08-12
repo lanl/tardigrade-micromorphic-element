b3c8f588ea506654e063d93a411f87385f5cb651
Nathan Andrew Miller <nathanm@lanl.gov>
2017-08-11 23:06:31 -0600

    Added additionl tests

--- src/cpp/tests/tensor/test_tensor.cpp
+++ src/cpp/tests/tensor/test_tensor.cpp
@@ -23,9 +23,34 @@
 #include <fstream>
 #include <numeric>
 #include <vector>
-#include <cstdarg>
+//#include <cstdarg>
 #include <Eigen/Dense>
 #include <tensor.h>
+
+int test_tensor_functionality(std::ofstream &results){
+    /*!====================================
+    |       test_tensor_functionality   |
+    =====================================
+    
+    A test of some of the basic tensor functionality. 
+    This should show that tensors of different orders 
+    can be generated and manipulated.*/
+
+    std::vector< int > v_shape;
+    v_shape.resize(1);
+    v_shape[0] = 6;
+    
+    std::cout << "Forming tensor\n";
+    tensor::Tensor V = tensor::Tensor(v_shape);
+    
+    std::cout << "V is of size " << V.data.rows() << "x" << V.data.cols() << "\n";
+    
+    std::cout << "V.data:\n" << V.data << "\n";
+    
+    V(2) = -7;
+    
+    std::cout << "V.data:\n" << V.data << "\n";
+}
 
 int main(){
     /*!==========================
@@ -38,5 +63,14 @@ int main(){
     False if the test passes or fails respectively.*/
     
     std::cout << "Compiled successfully";
+    
+    std::ofstream results;
+    //Open the results file
+    results.open ("results.tex");
+    results << "Writing this to a file.\n";
+        
+    //!Run the test functions
+    test_tensor_functionality(results);
+    results.close();
 }
 
