dd628a3ba83901de52e28f25684caa44ae26e9cc
Nathan Miller <Nathan.A.Mill@gmail.com>
2017-11-14 14:11:40 -0700

    Made modifications for different installations

--- config.py
+++ config.py
@@ -2,5 +2,20 @@ class Configuration(object):
     """A class which defines the configuration used for the setup"""
 
     def __init__(self):
+        
+        ### Define the path to the eigen library ###
         self.eigen    = "/path/to/eigen"
+        
+        #Uncomment for Nathan Miller's laptop
+        #self.eigen    = "g++"
+
+        #Uncomment for nami2227@soils.colorado.edu location
+        #self.eigen    = "/data/home/students/nami2227/Code/Eigen/eigen3.3.4"
+        
+        ### Define the path to the compiler library ###
         self.compiler = "/path/to/compiler"
+        
+        #Uncomment for Nathan Miller's laptop
+
+        #Uncomment for nami2227@soils.colorado.edu location
+        #self.compiler = "/data/home/students/nami2227/Code/gcc/gcc-7.2.0/bin/g++"
