c04073e79fa129689ae9463cfd407d7a9976a5d7
Nathan Miller <Nathan.A.Mill@gmail.com>
2017-11-14 14:53:42 -0700

    Added repr string for the Configuration class

--- config.py
+++ config.py
@@ -23,3 +23,22 @@ class Configuration(object):
 
         ### Define the make command
         self.make = "make"
+
+    def __repr__(self):
+        output_string    = "\n######################################\n"+\
+                           "###     Configuration Settings     ###\n"+\
+                           "######################################\n\n"+\
+                           "Configuration settings for the\n"+\
+                           "required settings for the micromorphic\n"+\
+                           "finite element.\n"
+        eigen_string     = "Eigen Library: " + str(self.eigen) + "\n"
+        compiler_string  = "Compiler Command: " + str(self.compiler) + "\n"
+        make_string      = "Make Command: " + str(self.make) + "\n"
+
+        output_string   += "\n### Libraries ###\n\n"+eigen_string+\
+                           "\n### Compiler Settings ###\n\n"+compiler_string+\
+                           "\n### Make Settings ###\n\n"+make_string
+        return output_string
+
+if __name__=="__main__":
+    print Configuration()
