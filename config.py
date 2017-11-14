9e6fe9c25bac19eb5cb2a80a75f7bfc1b6e8cf73
Nathan Miller <Nathan.A.Mill@gmail.com>
2017-11-14 14:05:12 -0700

    Initial commit

---
+++ config.py
@@ -0,0 +1,6 @@
+class Configuration(object):
+    """A class which defines the configuration used for the setup"""
+
+    def __init__(self):
+        self.eigen    = "/path/to/eigen"
+        self.compiler = "/path/to/compiler"
