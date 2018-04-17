2765288da76481830774c12f86c081392be52661
Nathan Andrew Miller <nathanm@lanl.gov>
2018-04-17 16:04:39 -0600

    Initial work to define force jacobians

--- src/cpp/balance_equations.cpp
+++ src/cpp/balance_equations.cpp
@@ -15,10 +15,11 @@
 #include <Eigen/Dense>
 #include <Eigen/Sparse>
 #include <deformation_measures.h>
+#include <balance_equations.h>
 
 namespace balance_equations{
 
-    void compute_internal_force(const double (&dNdx)[3], const double (&cauchy)[9], double (&fint)[3]){
+    void compute_internal_force(const double (&dNdx)[3], const Vector_9 &cauchy, double (&fint)[3]){
         /*!================================
         |    compute_internal_force    |
         ================================
@@ -28,9 +29,9 @@ namespace balance_equations{
 
         */
 
-        fint[0] = dNdx[0]*cauchy[1] + dNdx[1]*cauchy[8] + dNdx[2]*cauchy[7];
-        find[1] = dNdx[0]*cauchy[5] + dNdx[1]*cauchy[1] + dNdx[2]*cauchy[6];
-        find[2] = dNdx[0]*cauchy[4] + dNdx[1]*cauchy[3] + dNdx[2]*cauchy[2];
+        fint[0] = dNdx[0]*cauchy[0] + dNdx[1]*cauchy[8] + dNdx[2]*cauchy[7];
+        fint[1] = dNdx[0]*cauchy[5] + dNdx[1]*cauchy[1] + dNdx[2]*cauchy[6];
+        fint[2] = dNdx[0]*cauchy[4] + dNdx[1]*cauchy[3] + dNdx[2]*cauchy[2];
 
         return;
     }
@@ -68,16 +69,95 @@ namespace balance_equations{
         return;
     }
 
-    void compute_internal_stress(const double &N, const double (&dNdx)[3], const double (&cauchy)[9], const double (&s)[9], const double (&m)[27], double (&sint)[9]){
+    void compute_internal_couple(const double &N, const double (&dNdx)[3], const Vector_9 &cauchy, const Vector_9 &s, const Vector_27 &m, double (&cint)[9]){
         /*!=================================
-        |    compute_internal_stress    |
+        |    compute_internal_couple    |
         =================================
 
-        Compute the internal stress
+        Compute the internal couple.
 
         */
 
+        //Add the contributions of the cauchy stress and symmetric stress
+        for (int i=0; i<9; i++){
+            cint[i] = N*(cauchy[i]-s[i]);
+        }
+        
+        //Add the contributions of the gradient of the higher order couple stress
+        cint[0] -= dNdx[0]*m[0]+dNdx[1]*m[9]+dNdx[2]*m[18];
+        cint[5] -= dNdx[0]*m[8]+dNdx[1]*m[17]+dNdx[2]*m[26];
+        cint[4] -= dNdx[0]*m[7]+dNdx[1]*m[16]+dNdx[2]*m[25];
+        cint[8] -= dNdx[0]*m[5]+dNdx[1]*m[14]+dNdx[2]*m[23];
+        cint[1] -= dNdx[0]*m[1]+dNdx[1]*m[10]+dNdx[2]*m[19];
+        cint[3] -= dNdx[0]*m[6]+dNdx[1]*m[15]+dNdx[2]*m[24];
+        cint[7] -= dNdx[0]*m[4]+dNdx[1]*m[13]+dNdx[2]*m[22];
+        cint[6] -= dNdx[0]*m[3]+dNdx[1]*m[12]+dNdx[2]*m[21];
+        cint[2] -= dNdx[0]*m[2]+dNdx[1]*m[11]+dNdx[2]*m[20];
+        
         return;
     }
+    
+    void compute_body_couple(const double &N, const double &density, const double (&l)[9], double (&cb)[9]){
+        /*!=============================
+        |    compute_body_couple    |
+        =============================
+        
+        Compute the body couple term.
+        
+        */
+        
+        cb[0] = N*density*l[0];
+        cb[1] = N*density*l[1];
+        cb[2] = N*density*l[2];
+        cb[3] = N*density*l[6];
+        cb[4] = N*density*l[7];
+        cb[5] = N*density*l[8];
+        cb[6] = N*density*l[3];
+        cb[7] = N*density*l[4];
+        cb[8] = N*density*l[5];
+        
+        return;
+    }
+    
+    void compute_kinematic_couple(const double &N, const double &density, const double (&omega)[9], double (&ckin)[9]){
+        /*!=============================
+        |    compute_body_couple    |
+        =============================
+        
+        Compute the body couple term.
+        
+        */
+        
+        cb[0] = N*density*omega[0];
+        cb[1] = N*density*omega[1];
+        cb[2] = N*density*omega[2];
+        cb[3] = N*density*omega[6];
+        cb[4] = N*density*omega[7];
+        cb[5] = N*density*omega[8];
+        cb[6] = N*density*omega[3];
+        cb[7] = N*density*omega[4];
+        cb[8] = N*density*omega[5];
+        
+        return;
+    }
+    
+    void compute_internal_force_jacobian(const int &dof_num, const double &N, const double (&dNdx)[3], const Matrix_9x9 &DcauchyDF, const Matrix_9x9 &DcauchyDphi, const Matrix_9x27 &DcauchyDgrad_phi, const Matrix_27x9 &dFdgrad_u){
+        /*!=========================================
+        |    compute_internal_force_jacobian    |
+        =========================================
+        
+        Compute the internal force jacobian for a given DOF.
+        
+        Note that the DOF are expected to be organized
+        
+        dof_num:  0,  1,  2,     3,     4,     5,     6,     7,     8,     9,    10,    11
+            dof: u1, u2, u3, phi11, phi22, phi33, phi23, phi13, phi12, phi32, phi31, phi21
+            
+        
+        
+        */
+        
+        //!Total derivatives
 
+    }
 }
--- src/cpp/balance_equations.h
+++ src/cpp/balance_equations.h
@@ -15,18 +15,31 @@
 #ifndef BALANCE_EQUATIONS_H
 #define BALANCE_EQUATIONS_H
 
+#include <Eigen/Dense>
+#include <Eigen/Sparse>
+#include <deformation_measures.h>
+
 namespace balance_equations{
 
     //Forces from the balance of linear momentum
-    void compute_internal_force(const double (&dNdx)[3], const double (&cauchy)[9], double (&fint)[3]);
+    void compute_internal_force(const double (&dNdx)[3], const Vector_9 &cauchy, double (&fint)[3]);
 
     void compute_body_force(const double &N, const double &density, const (&b)[3], double (&fb)[3]);
 
     void compute_kinematic_force(const double &N, const double &density, const (&a)[3], double (&fkin)[3]);
 
     //Stresses from the balance of first moment of momentum
-    void compute_internal_stress(const double &N, const double (&dNdx)[3], const double (&cauchy)[9], const double (&s)[9], const double (&m)[27], double (&sint)[9]);
+    void compute_internal_couple(const double &N, const double (&dNdx)[3], const Vector_9 &cauchy, const Vector_9 &s, const Vector_27 &m, double (&cint)[9]);
 
+    void compute_body_couple(const double &N, const double &density, const double (&l)[9], double (&cb)[9]);
+    
+    void compute_kinematic_couple(const double &N, const double &density, const double (&omega)[9], double (&ckin)[9]);
+    
+    //Compute the jacobians of the balance of linear momentum w.r.t. the indicated displacement dof
+    void compute_internal_force_jacobian(const int &dof_num, const double &N, const double (&dNdx)[3], const Matrix_9x9 &dcauchydF, const Matrix_9x9 &dcauchydchi, const Matrix_9x27 &dcauchydgrad_chi, const Matrix_27x9 &dgrad_chidF, double (&dfintdU)[3,12]);
+    
+    
+    //Compute the jacobians of the balance of first moment of momentum
 }
 
 #endif
