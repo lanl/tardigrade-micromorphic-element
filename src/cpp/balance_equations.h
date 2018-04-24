a395db03541b06ebf50defc292956512c36cfb28
Nathan Miller <Nathan.A.Mill@gmail.com>
2018-04-24 08:45:04 -0600

    Removed references to test_fxn and replaced it with N

--- src/cpp/balance_equations.h
+++ src/cpp/balance_equations.h
@@ -30,22 +30,22 @@ typedef Eigen::Matrix<double,27, 12> Matrix_27x12;
 namespace balance_equations{
 
     //Forces from the balance of linear momentum
-    void compute_internal_force(const double (&dtestdx)[3], const Vector_9 &cauchy, double (&fint)[3]);
+    void compute_internal_force(const double (&dNdx)[3], const Vector_9 &cauchy, double (&fint)[3]);
     
-    void compute_internal_force(const int &i, const double (&dtest_fxndx)[3], const Vector_9 &cauchy, double &fint_i);
+    void compute_internal_force(const int &i, const double (&dNdx)[3], const Vector_9 &cauchy, double &fint_i);
 
-    void compute_body_force(const double &test_fxn, const double &density, const double (&b)[3], double (&fb)[3]);
+    void compute_body_force(const double &N, const double &density, const double (&b)[3], double (&fb)[3]);
     
-    void compute_body_force(const int &i, const double &test_fxn, const double &density, const double (&b)[3], double &fb);
+    void compute_body_force(const int &i, const double &N, const double &density, const double (&b)[3], double &fb);
 
-    void compute_kinematic_force(const double &test_fxn, const double &density, const double (&a)[3], double (&fkin)[3]);
+    void compute_kinematic_force(const double &N, const double &density, const double (&a)[3], double (&fkin)[3]);
     
-    void compute_kinematic_force(const int &i, const double &test_fxn, const double &density, const double (&a)[3], double &fkin);
+    void compute_kinematic_force(const int &i, const double &N, const double &density, const double (&a)[3], double &fkin);
 
     //Stresses from the balance of first moment of momentum
-    void compute_internal_couple(const double &test_fxn, const double (&dtest_fxndx)[3], const Vector_9 &cauchy, const Vector_9 &s, const Vector_27 &m, double (&cint)[9]);
+    void compute_internal_couple(const double &N, const double (&dNdx)[3], const Vector_9 &cauchy, const Vector_9 &s, const Vector_27 &m, double (&cint)[9]);
     
-    void compute_internal_couple(const int &i, const int &j, const double &test_fxn, const double (&dtest_fxndx)[3], const Vector_9 &cauchy, const Vector_9 &s, const Vector_27 &m, double &cint_ij);
+    void compute_internal_couple(const int &i, const int &j, const double &N, const double (&dNdx)[3], const Vector_9 &cauchy, const Vector_9 &s, const Vector_27 &m, double &cint_ij);
 
     void compute_body_couple(const double &N, const double &density, const double (&l)[9], double (&cb)[9]);
     
