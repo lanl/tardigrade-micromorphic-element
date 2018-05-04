809de023505c385c00e57b1174e1b69af04d9742
Nathan Andrew Miller <nathanm@lanl.gov>
2018-05-04 09:35:13 -0600

    Added tests of the efficiency of contraction techniques

---
+++ src/cpp/efficiency_tests/tensor_multiplication.cpp
@@ -0,0 +1,77 @@
+#include <Eigen/Dense>
+#include <iostream>
+#include <chrono>
+typedef std::chrono::high_resolution_clock Clock;
+
+//Global variables
+const double sot_to_voigt[3][3] = {{0, 5, 4},
+                                   {8, 1, 3},
+                                   {7, 6, 2}};
+
+void explicit_loop(const Eigen::Matrix3d &A, const Eigen::Matrix3d &B, const Eigen::Matrix<double, 9, 9> C, Eigen::Matrix<double, 9, 9> &D){
+    /*
+    =======================
+    |    explicit_loop    |
+    =======================
+    
+    Compute the contraction explicitly.
+    
+    */
+    
+    int I;
+    int J;
+    int M;
+    
+    for (int i=0; i<3; i++){
+        for (int j=0; j<3; j++){
+            I = sot_to_voigt[i][j];
+            
+            for (int k=0; k<3; k++){
+                for (int l=0; l<3; l++){
+                    J = sot_to_voigt[k][l];
+                    
+                    D(I,J) = 0.;
+                    
+                    for (int m=0; m<3; m++){
+                        for (int n=0; n<3; n++){
+                            M = sot_to_voigt[m][n];
+                            
+                            D(I,J) += A(i,m)*C(M,J)*B(j,n);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+int main(){
+    /*
+    ==============
+    |    main    |
+    ==============
+    
+    Tests different methods for computing tensor multiplications and reports the 
+    relative speeds.
+    */
+    
+    Eigen::Matrix3d A;
+    A = Eigen::Matrix3d::Random();
+    
+    Eigen::Matrix3d B;
+    B = Eigen::Matrix3d::Random();
+    
+    Eigen::Matrix<double, 9, 9> C;
+    C = Eigen::Matrix<double, 9, 9>::Random();
+    
+    Eigen::Matrix<double, 9, 9> D;
+    
+    auto t0 = Clock::now();
+    auto t1 = Clock::now();
+    
+    t0 = Clock::now();
+    explicit_loop(A,B,C,D);
+    t1 = Clock::now();
+    std::cout << "Explicit Loop: " << std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count() << "\n";
+    return 1;
+}
