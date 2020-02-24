1526a95cd667a90c0860bc26d6e60043cc86c8f5
Nathan Miller <nathanm@lanl.gov>
2020-02-23 21:13:35 -0700

    Initial commit

---
+++ ReadMe.mk
@@ -0,0 +1,7 @@
+# Micromorphic Linear Elasticity
+
+Developed by: Nathan Miller
+email: nathanm@lanl.gov
+
+An implementation of a quadratic form micromorphic linear elasticity. This is 
+based off of notes by Richard Regueiro and Nathan Miller's dissertation.
